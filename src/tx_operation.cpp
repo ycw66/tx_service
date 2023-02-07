839d7d18f2cea1cfeef892a1618e4f98afbb088b
xiaolin <xiaolin0310@gmail.com>
2023-02-07 11:40:02 +0800

    UpsertTableOp should reset acquire_all_intent_op_.protocol_ to OCC. (#461)

--- src/tx_operation.cpp
+++ src/tx_operation.cpp
@@ -2006,7 +2006,7 @@ void UpsertTableOp::Reset(const std::string_view table_name_str,
     acquire_all_intent_op_.table_name_ = &catalog_ccm_name;
     acquire_all_intent_op_.key_ = &table_key_;
     acquire_all_intent_op_.cc_op_ = CcOperation::ReadForWrite;
-    acquire_all_intent_op_.protocol_ = CcProtocol::OccRead;
+    acquire_all_intent_op_.protocol_ = CcProtocol::OCC;
 
     post_all_intent_op_.table_name_ = &catalog_ccm_name;
     post_all_intent_op_.key_ = &table_key_;
