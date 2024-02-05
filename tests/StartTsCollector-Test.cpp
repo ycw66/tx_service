ad6688def008a293556ddd5ac2c1b72747ddd466
liunyl <107381948+liunyl@users.noreply.github.com>
2024-02-05 16:38:54 +0800

    link mimalloc static to libtxserivce.a (#886)

--- tests/StartTsCollector-Test.cpp
+++ tests/StartTsCollector-Test.cpp
@@ -39,8 +39,18 @@ TEST_CASE("TxStartTsCollector GlobalMinSiTxStartTs", "[start-ts-collector]")
         std::pair<std::string, uint32_t>("core_num", core_num));
     tx_service_conf.insert(
         std::pair<std::string, uint32_t>("checkpointer_interval", 10));
+    tx_service_conf.insert(
+        std::pair<std::string, uint32_t>("checkpointer_delay_seconds", 0));
     tx_service_conf.insert(std::pair<std::string, uint32_t>(
         "collect_active_tx_ts_interval_seconds", 2));
+    tx_service_conf.insert(
+        std::pair<std::string, uint32_t>("node_memory_limit_mb", 1000));
+    tx_service_conf.insert(
+        std::pair<std::string, uint32_t>("node_log_limit_mb", 1000));
+    tx_service_conf.insert(
+        std::pair<std::string, uint32_t>("realtime_sampling", 0));
+    tx_service_conf.insert(
+        std::pair<std::string, uint32_t>("rep_group_cnt", 3));
 
     uint16_t log_server_port = 8602;
     std::vector<uint16_t> log_instance_ports = ports;
